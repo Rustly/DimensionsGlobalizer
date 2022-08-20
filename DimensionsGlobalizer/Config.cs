71297de0020f021d685ea46e527f47fa2133b103
Rustly <43079937+Rustly@users.noreply.github.com>
2022-08-19 20:14:58 -0500

    Initial commit

---
+++ DimensionsGlobalizer/Config.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DimensionsGlobalizer
+{
+    public class Config
+    {
+        private static readonly string _path = Path.Combine(TShockAPI.TShock.SavePath, "realmsclient.json");
+
+        public static Config Read()
+        {
+            try
+            {
+                var res = new Config();
+                if (File.Exists(_path))
+                    res = JsonConvert.DeserializeObject<Config>(File.ReadAllText(_path));
+                File.WriteAllText(_path, JsonConvert.SerializeObject(res, Formatting.Indented));
+                return res;
+            }
+            catch (Exception ex)
+            {
+                TShockAPI.TShock.Log.Error(ex.ToString());
+            }
+
+            return null;
+        }
+
+        public bool EnableItemBanGlobalization { get; set; } = true;
+
+        public bool EnableProjectileBanGlobalization { get; set; } = true;
+
+        public bool EnableTileBanGlobalization { get; set; } = true;
+
+        public bool EnableGroupGlobalization { get; set; } = true;
+
+        public bool EnableBanGlobalization { get; set; } = true;
+
+        public bool EnableUserGlobalization { get; set; } = true;
+
+        public string GlobalDatabaseConnection { get; set; } = "Server=localhost; Port=3306; Database=global; Uid=username; Pwd=assword;";
+    }
+}
---
+++ DimensionsGlobalizer/DimensionsGlobalizer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaApi.Server;
+using Terraria;
+using TShockAPI;
+
+namespace DimensionsGlobalizer
+{
+    public class DimensionsGlobalizer : TerrariaPlugin
+    {
+        private Config config;
+
+        public DimensionsGlobalizer(Main game) : base(game)
+        {
+            config = Config.Read();
+        }
+
+        public override void Initialize()
+        {
+            if (TShock.Config.Settings.StorageType.ToLowerInvariant() != "mysql")
+                throw new Exception("Globalization is only allowed if config.storagetype is set to mysql.");
+
+            var db = new MySql.Data.MySqlClient.MySqlConnection(config.GlobalDatabaseConnection);
+
+            if (config.EnableUserGlobalization)
+                TShock.UserAccounts = new TShockAPI.DB.UserAccountManager(db);
+            if (config.EnableBanGlobalization)
+                TShock.Bans = new TShockAPI.DB.BanManager(db);
+            if (config.EnableGroupGlobalization)
+                TShock.Groups = new TShockAPI.DB.GroupManager(db);
+            if (config.EnableProjectileBanGlobalization)
+                TShock.ProjectileBans = new TShockAPI.DB.ProjectileManagager(db);
+            if (config.EnableTileBanGlobalization)
+                TShock.TileBans = new TShockAPI.DB.TileManager(db);
+            if (config.EnableItemBanGlobalization)
+                TShock.ItemBans.DataModel = new TShockAPI.DB.ItemManager(db);
+
+            TShockAPI.Hooks.GeneralHooks.ReloadEvent += GeneralHooks_ReloadEvent;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                TShockAPI.Hooks.GeneralHooks.ReloadEvent -= GeneralHooks_ReloadEvent;
+            }
+
+            base.Dispose(disposing);
+        }
+
+        private void GeneralHooks_ReloadEvent(TShockAPI.Hooks.ReloadEventArgs e)
+        {
+            config = Config.Read();
+        }
+    }
+}
